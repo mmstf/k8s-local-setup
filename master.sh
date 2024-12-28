328112f1475e6ba37784c44f3b8bd2b313813965
mmstf <mustafobey99@mail.ru>
2024-12-29 02:40:50 +0500

    sh: updated k8s.list

--- master.sh
+++ master.sh
@@ -15,7 +15,7 @@ sudo sysctl --system
 
 # docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io
